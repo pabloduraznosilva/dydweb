abb3f542fe8afd7c3ff43422de02cd5f8c626c76
pabloduraznosilva <61339044+pabloduraznosilva@users.noreply.github.com>
2020-02-21 23:00:38 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Examen 2E1</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container">
+  Hola mundo
+  </div>
+</body>
+</html>
